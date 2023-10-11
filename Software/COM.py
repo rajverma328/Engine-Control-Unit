61d84b1ca3f1a89fd467061b4863f72ffe7aaa61
Raj Verma <jprajverma328@gmail.com>
2023-10-11 16:25:09 +0530

    Commit 9:55 PM 12 October 2023

--- Software/COM.py
+++ Software/COM.py
@@ -1,25 +1,36 @@
+import sys
 import glob
-import platform
+import serial
 
-# Get the current operating system
-current_os = platform.system()
 
-# Determine the prefix for serial ports based on the operating system
-if current_os == "Windows":
-    port_prefix = "COM"
-elif current_os == "Linux":
-    port_prefix = "/dev/tty"
-elif current_os == "Darwin":  # macOS
-    port_prefix = "/dev/cu."
-# Define the pattern to search for serial ports
-serial_port_pattern = '/dev/ttyS*'  # You can also use '/dev/ttyUSB*' or other patterns
+def serial_ports():
+    """ Lists serial port names
 
-# Use glob to find matching serial ports
-serial_ports = glob.glob(serial_port_pattern)
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(256)]
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
 
-# Print the list of available serial ports
-if not serial_ports:
-    print("No serial ports found.")
-else:
-    for port in serial_ports:
-        print(port)
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+if __name__ == '__main__':
+    print(serial_ports())
